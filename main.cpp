285ae0f52ca87625078f0aa7acec879666ebe22a
nazaryout111-code <nazaryout111@gmail.com>
2026-07-09 16:20:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include "Reg.h"
+#include "Zmiyka.h"
+#include "Calc.h"
+#include <iostream>
 
+int main()
+{
+
+    std::cout << "Доброго дня! Вітаємо в Операційній системі версії 0.1 Beta\n";
+    std::cout << "Вам потрібно зареєструватися\n";
+
+    password(); 
+}
